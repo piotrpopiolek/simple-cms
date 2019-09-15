c78c5b01b7d0107ffc1ca444ef51c1432b6100be
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-09-15 15:13:55 +0200

    Add twig

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
+    public function index()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
