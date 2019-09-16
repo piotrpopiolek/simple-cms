dc619da0dd9a98ef7db57751dfd0aafa52a1751b
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-09-16 22:00:39 +0200

    install require doctrine maker

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -14,6 +14,8 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        return $this -> render('articles/index.html.twig');
+        $articles = ['Article 1', 'Article 2', 'Article 3'];
+
+        return $this -> render('articles/index.html.twig', array('articles' => $articles));
     }
 }
