968701e6bf1350039a195d4514a52c24ba747076
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-09-18 00:10:12 +0200

    Show article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,34 +13,45 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ArticleController extends Controller
 {
+    
     /**
      * @Route("/")
      * @Method({"GET"})
      */
     public function index()
     {
-        $articles = ['Article 1', 'Article 2', 'Article 3'];
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this -> render('articles/index.html.twig', array('articles' => $articles));
     }
 
     /**
-    * @Route("/article/save")
+    * @Route("/article/{id}")
     */
-    public function save() {
-    
-        $entityManager = $this->getDoctrine()->getManager();
-    
-        $article = new Article();
-    
-        $article->setTitle('Article Two');
-    
-        $article->setBody('This is the body for article two');
-    
-        $entityManager->persist($article);
-    
-        $entityManager->flush();
-    
-        return new Response('Saved an article with the id of  '.$article->getId());
+    public function show($id){
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+        
+
+        return $this->render('articles/show.html.twig', array('article' => $article));
     }
+
+    // /**
+    // * @Route("/article/save")
+    // */
+    // public function save() {
+    
+    //     $entityManager = $this->getDoctrine()->getManager();
+    
+    //     $article = new Article();
+    
+    //     $article->setTitle('Article Two');
+    
+    //     $article->setBody('This is the body for article two');
+    
+    //     $entityManager->persist($article);
+    
+    //     $entityManager->flush();
+    
+    //     return new Response('Saved an article with the id of  '.$article->getId());
+    // }
 }
