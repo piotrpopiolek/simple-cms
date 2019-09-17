d24350b383d2bae22cf728b3c15312713e557c45
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-09-17 22:20:21 +0200

    Add hard code save

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,10 +1,15 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+
 
 class ArticleController extends Controller
 {
@@ -17,5 +22,25 @@ class ArticleController extends Controller
         $articles = ['Article 1', 'Article 2', 'Article 3'];
 
         return $this -> render('articles/index.html.twig', array('articles' => $articles));
+    }
+
+    /**
+    * @Route("/article/save")
+    */
+    public function save() {
+    
+        $entityManager = $this->getDoctrine()->getManager();
+    
+        $article = new Article();
+    
+        $article->setTitle('Article Two');
+    
+        $article->setBody('This is the body for article two');
+    
+        $entityManager->persist($article);
+    
+        $entityManager->flush();
+    
+        return new Response('Saved an article with the id of  '.$article->getId());
     }
 }
